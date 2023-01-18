5a0d9a73cbed9f0bfea4534492686501e2e72a89
shellyqinyu <shelly@processis.org>
2023-01-18 03:06:51 -0500

    ch9.5.1

--- bernbeta.R
+++ bernbeta.R
@@ -160,7 +160,7 @@ plotPost(thetaDiff , xlab=expression(theta[1]-theta[2]),compVal=0.0,
 
 #8.8.1
 nInt = 501
-theta1=seq(from((1/nINt)/2),to=(1-((1/nInt)/2)),by=(1/nInt))
+theta1=seq(from=((1/nInt)/2),to=(1-((1/nInt)/2)),by=(1/nInt))
 theta2=theta1
 
 priorName=c("Beta","Ripples","Null","Alt")[1]
@@ -206,7 +206,7 @@ plotFIleName=paste("BernTwoGrid",priorName,".eps",sep="")
 credib=.95
 
 rotate=(-25)
-tile=25
+tilt=25
 parallelness=5.0
 shadeval=0.05
 perspcex=0.7
@@ -227,7 +227,7 @@ par(mai=c(0.4,0.4,0.2,0.05))
 par(pty="s")
 
 
-persp(thetal[thindex1],theta2[thindex2],prior[thindex1,thindex2],
+persp(theta1[thindex1],theta2[thindex2],prior[thindex1,thindex2],
       xlab="theta1",ylab = "theta2",main = "Prior",cex=perspcex,lwd=0.1,
       xlim=c(0,1),ylim=c(0,1),zlim=c(0,zmax),zlab="p(ti,t2)",
       theta=rotate,phi=tilt,d=parallelness,shade=shadeval)
@@ -270,8 +270,8 @@ maxpost=which(posterior==max(posterior),arr.ind=TRUE)
 if(theta1[maxpost[1]]>0.5){textxpos=0;xadj=0
 }else{textxpos=1;xadj=1}
 
-if(theta2[maxpost[2]]>0.5){textxpos=0;xadj=0
-}else{textxpos=1;xadj=1}
+if(theta2[maxpost[2]]>0.5){textypos=0;yadj=0
+}else{textypos=1;yadj=1}
 
 text(textxpos,textypos,ces=1.5,
      bquote("p(D)"*.(signif(pData,3))),
@@ -287,3 +287,108 @@ contour(theta1[thindex1],theta2[thindex2],posterior[thindex1,thindex2],
 
 wantSaveGraph=T
 if(wantSaveGraph){dev.copy2eps(file=plotFileName)}
+
+
+
+#9.5.1
+
+graphics.off()
+rm(list=ls(all=TRUE))
+library(BRugs)
+
+modelString=
+model{
+  for(t in 1:nTrialTotal){
+    y[t]^dbern(theta[coin[t]])
+  }
+  for(j in 1:nCions){
+    theta[j]^dbeta(a,b)I(0.0001,0.9999)
+  }
+  a<-mu*kappa
+  b<-(1.0-mu)*kappa
+  mu^dbeta(Amu,Bmu)
+  kappa^dgamma(Skappa,Rkappa)
+  Amu<-2.0
+  Bmu<-2.0
+  Skappa<-pow(10,2)/pow(10,2)
+  Rkappa<-10/pow(10,2)
+}
+
+.temp=file("model.txt","w");writeLines(modelString,con=.tem);close(.temp)
+modelCheck("model.txt")
+
+N=c(5,5,5,5,5)
+z=c(1,1,1,1,5)
+
+coin=NULL;y=NULL
+for(cionIdx in 1:length(N)){
+  cion=c(cion,rep(cionIdx,N[cionIdx]))
+  y=c(y,rep(1,z[cionIdx]),rep(0,N[cionIdx]-z[cionIdx]))
+}
+  
+nTrialTotal=length(y)
+nCoins=length(unique(coin))
+dataList=list(
+  y=y,
+  coin=coin,
+  nTrialTotal=nTrialTotal,
+  nCoins=nCoins
+)
+  
+
+modelData(bugsData(dataList))
+
+
+nChain=3
+modelCompile(numChain=nChain)
+modelGenInits()
+
+
+burninSteps=1000
+modelUpdata(burninSteps)
+samplesSet(c("mu","kappa","theta"))
+nPerChain=1000
+modelUpdata(nPerChain,thin=10)
+
+source("plotChains.R")
+plotChains("mu",saveplots=F)
+plotChains("kappa",saveplots=F)
+plotChains("theta[1]",saveplots=F)
+
+muSample=samplesSample("mu")
+kappaSample=samplesSample("kappa")
+thetaSample=matrix(0,nrow=nCoins,ncol=nChains*nPerChain)
+for(coinIdx in 1:nCoins){
+  nodeName=paste("theta[",coinIdx,"]",sep="")
+  thetaSample[cionIdx,]=samplesSample(nodeName)
+}
+
+
+source("plotPost.R")
+if(nCoin<=5){
+  windows(3.2*3,2.5*(1+nCoins))
+  layout(matrix(1:(3*(nCoins+1)),nrow=(nCoins+1),byrow=T))
+  par(mar=c(2.95,2.95,1.0,0),mgp=c(1.35,0.35,0),oma=c(0.1,0.1,0.1,0.1))
+  nPtsToPlot=500
+  plotIdx=floor(seq(1,length(muSample),length=nPtsToPlot))
+  kPltLim=signif(quantile(kappaSample,p=c(.01,.99)),4)
+}
+plot(muSample[plotIdx],kappaSample[plotIdx],type="p",ylim=kPltLim,
+       xlim=c(0,1),xlab=expression(mu),ylab=expression(kappa),cex.lab=1.5)
+plotPost(muSample,xlab="mu",xlim=c(0,1),main="",breaks=20)
+plotPost(kappaSample,xlab="kappa",main="",breaks=0,HDItextPLace=.6)
+for(coinIdx in 1:nCoins){
+  plotPost(thetaSample[cionIdx,],xlab=paste("theta",coinIdx,sep=""),
+           xlim=c(0,1),main="",breaks=20,HDItextPlace=.3)
+  
+  plot(thetaSample[cionIdx,plotIdx],muSample[plotIdx],type="p",
+           xlim=c(0,1),ylim=c(0,1),cex.lab=1.5,
+           xlab=bquote(theta[.(cionIdx)]),ylab=expression(mu))
+  
+  plot(thetaSample[cionIdx,plotIdx],kappaSample[plotIdx],type="p",
+       xlim=c(0,1),ylim=kPltLim,cex.lab=1.5,
+       xlab=bquote(theta[.(cionIdx)]),ylab=expression(kappa))
+  
+}
+
+dev.copy2eps(file=paste("BernBetaMuKappaBugs",paste(z,collapse=""),".eps",sep=""))
