cdf9552eb9fdd9babd212e2c4938105c62409dad
Debian Live user <edmondsksung@hotmail.com>
2023-11-02 16:27:08 +0800

    edmond

---
+++ 201relearnTitanic.R
@@ -0,0 +1,361 @@
+# 展示GitHUB 给 shelly
+## Load raw data
+train <- read.csv("train.csv", header = TRUE)
+test <- read.csv("test.csv", header = TRUE)
+# Add a "Survived" variable to the test set to allow for combining data sets
+test.Survived <- data.frame(Survived = rep("None", nrow(test)), test[,])
+# Combine data set
+data.combined <- rbind(train, test.Survived)
+# A bit about R data types (e.g., factors)
+str(data.combined)
+data.combined$Survived <- as.factor(data.combined$Survived)
+View(train)
+data.combined$Pclass <- as.factor(data.combined$Pclass)
+# Take a look at gross survival rates
+table(data.combined$Survived)
+# Distribution across classes
+table(data.combined$Pclass)
+# Load up ggplot2 package to use for visualizations
+library(ggplot2)
+# Hypothesis - Rich folks survived at a higer rate
+train$Pclass <- as.factor(train$Pclass)
+ggplot(train, aes(x = Pclass, fill = factor(Survived))) +
+     geom_bar() +
+     xlab("Pclass") +
+     ylab("Total Count") +
+     labs(fill = "Survived") 
+View(train)
+# Examine the first few names in the training data set
+head(as.character(train$Name))
+# How many unique names are there across both train & test?
+length(unique(as.character(data.combined$Name)))
+# Two duplicate names, take a closer look
+# First, get the duplicate names and store them as a vector
+dup.names <- as.character(data.combined[which(duplicated(as.character(data.combined$Name))), "Name"])
+# Next, take a look at the records in the combined data set
+data.combined[which(data.combined$Name %in% dup.names),]
+# What is up with the 'Miss.' and 'Mr.' thing?
+library(stringr)
+# Any correlation with other variables (e.g., sibsp)?
+misses <- data.combined[which(str_detect(data.combined$Name, "Miss.")),]
+misses[1:5,]
+# Hypothesis - Name titles correlate with age
+mrses <- data.combined[which(str_detect(data.combined$Name, "Mrs.")), ]
+mrses[1:5,]
+View(data.combined)
+males <- data.combined[which(data.combined$Sex == "male"), ]
+males[1:5,]
+# Create a utility function to help with title extraction
+extractTitle <- function(name) {
+     name <- as.character(name)
+  
+      if (length(grep("Miss.", name)) > 0) {
+             return ("Miss.")
+         } else if (length(grep("Master.", name)) > 0) {
+               return ("Master.")
+           } else if (length(grep("Mrs.", name)) > 0) {
+                 return ("Mrs.")
+             } else if (length(grep("Mr.", name)) > 0) {
+                  return ("Mr.")
+                } else {
+                    return ("Other")
+              }
+   }
+titles <- NULL
+for (i in 1:nrow(data.combined)) {
+      titles <- c(titles, extractTitle(data.combined[i,"Name"]))
+  }
+data.combined$title <- as.factor(titles)
+View(data.combined)
+ggplot(data.combined[1:891,], aes(x = title, fill = Survived)) +
+    geom_bar() +
+     facet_wrap(~Pclass) + 
+     ggtitle("Pclass") +
+     xlab("Title") +
+     ylab("Total Count") +
+     labs(fill = "Survived")
+# What's the distribution of females to males across train & test?
+table(data.combined$Sex)
+#
+#....
+#
+# Validate that "Master." is a good proxy for male children
+boys <- data.combined[which(data.combined$title == "Master."),]
+summary(boys$Age)
+# We know that "Miss." is more complicated, let's examine further
+misses <- data.combined[which(data.combined$title == "Miss."),]
+summary(misses$Age)
+
+ggplot(misses[misses$Survived != "None" & !is.na(misses$Age),], aes(x = Age, fill = Survived)) +
+  facet_wrap(~Pclass) +
+  geom_histogram(binwidth = 5) +
+  ggtitle("Age for 'Miss.' by Pclass") + 
+  xlab("Age") +
+  ylab("Total Count")
+# OK, appears female children may have different survival rate, 
+# could be a candidate for feature engineering later
+misses.alone <- misses[which(misses$SibSp == 0 & misses$Parch == 0),]
+summary(misses.alone$Age)
+length(which(misses.alone$Age <= 14.5))
+#
+#...
+#
+# Let's try some feature engineering. What about creating a family size feature?
+temp.sibsp <- c(train$SibSp, test$SibSp)
+temp.parch <- c(train$Parch, test$Parch)
+data.combined$family.size <- as.factor(temp.sibsp + temp.parch + 1)
+
+# Visualize it to see if it is predictive
+ggplot(data.combined[1:891,], aes(x = family.size, fill = Survived)) +
+  geom_bar() +
+  facet_wrap(~Pclass + title) + 
+  ggtitle("Pclass, Title") +
+  xlab("family.size") +
+  ylab("Total Count") +
+  ylim(0,300) +
+  labs(fill = "Survived")
+# Take a look at the ticket variable
+str(data.combined$Ticket)
+
+
+# Based on the huge number of levels ticket really isn't a factor variable it is a string. 
+# Convert it and display first 20
+data.combined$Ticket <- as.character(data.combined$Ticket)
+data.combined$Ticket[1:20]
+#
+#...
+#
+# Video #4 - Exploratory Modeling
+library(randomForest)
+#
+rf.label <- as.factor(train$Survived)
+# Train a Random Forest using pclass, title, & family.size
+rf.train.5 <- data.combined[1:891, c("Pclass", "title", "family.size")]
+
+set.seed(1234)
+rf.5 <- randomForest(x = rf.train.5, y = rf.label, importance = TRUE, ntree = 1000)
+rf.5
+varImpPlot(rf.5)
+# Video #5 - Cross Validation
+# for estimating our error rate on the test set (i.e., unseen data). This is
+# critical, for without this we are more likely to overfit. Let's start with a 
+# submission of rf.5 to Kaggle to see if our OOB error estimate is accurate.
+
+# Subset our test records and features
+test.submit.df <- data.combined[892:1309, c("Pclass", "title", "family.size")]
+
+# Make predictions
+rf.5.preds <- predict(rf.5, test.submit.df)
+table(rf.5.preds)
+# Write out a CSV file for submission to Kaggle
+submit.df <- data.frame(PassengerId = rep(892:1309), Survived = rf.5.preds)
+write.csv(submit.df, file = "RF_SUB_20200201_1.csv", row.names = FALSE)
+#
+# Our submission scores 0.79426, but the OOB predicts that we should score 0.8159.
+# Let's look into cross-validation using the caret package to see if we can get
+# more accurate estimates
+library(caret)
+# Leverage caret to create 100 total folds, but ensure that the ratio of those
+# that survived and perished in each fold matches the overall training set. This
+# is known as stratified cross validation and generally provides better results.
+set.seed(2348)
+cv.10.folds <- createMultiFolds(rf.label, k = 10, times = 10)
+
+# Check stratification
+table(rf.label)
+table(rf.label[cv.10.folds[[33]]])
+# Set up caret's trainControl object per above.
+ctrl.1 <- trainControl(method = "repeatedcv", number = 10, repeats = 10,
+                       index = cv.10.folds)
+# Set seed for reproducibility and train
+set.seed(34324)
+rf.5.cv.1 <- train(x = rf.train.5, y = rf.label, method = "rf", tuneLength = 3,
+                   ntree = 1000, trControl = ctrl.1)
+# Check out results
+rf.5.cv.1
+#
+#...
+#
+# 5-fold CV isn't better. Move to 3-fold CV repeated 10 times. 
+set.seed(37596)
+cv.3.folds <- createMultiFolds(rf.label, k = 3, times = 10)
+ctrl.3 <- trainControl(method = "repeatedcv", number = 3, repeats = 10,
+                       index = cv.3.folds)
+set.seed(94622)
+rf.5.cv.3 <- train(x = rf.train.5, y = rf.label, method = "rf", tuneLength = 3,
+                   ntree = 64, trControl = ctrl.3)
+# Check out results
+rf.5.cv.3
+# Video #6 - Exploratory Modeling 2
+# Let's use a single decision tree to better understand what's going on with our
+# features. Obviously Random Forests are far more powerful than single trees,
+# but single trees have the advantage of being easier to understand.
+
+# Install and load packages
+#install.packages("rpart")
+#install.packages("rpart.plot")
+library(rpart)
+library(rpart.plot)
+#Create utility function
+rpart.cv <- function(seed, training, labels, ctrl) {
+  set.seed(seed)
+  # Leverage formula interface for training
+  rpart.cv <- train(x = training, y = labels, method = "rpart", tuneLength = 30, 
+                    trControl = ctrl)
+  return (rpart.cv)
+}
+# Grab features
+features <- c("Pclass", "title", "family.size")
+rpart.train.1 <- data.combined[1:891, features]
+
+# Run CV and check out results
+rpart.1.cv.1 <- rpart.cv(94622, rpart.train.1, rf.label, ctrl.3)
+rpart.1.cv.1
+
+# Plot
+prp(rpart.1.cv.1$finalModel, type = 0, extra = 1, under = TRUE)
+#
+#..
+#
+# Both rpart and rf confirm that title is important, let's investigate further
+table(data.combined$title)
+
+# Parse out last name and title
+data.combined[1:25, "Name"]
+
+name.splits <- str_split(data.combined$Name, ",")
+name.splits[1]
+last.names <- sapply(name.splits, "[", 1)
+last.names[1:10]
+
+# Add last names to dataframe in case we find it useful later
+data.combined$last.name <- last.names
+
+# Now for titles
+name.splits <- str_split(sapply(name.splits, "[", 2), " ")
+titles <- sapply(name.splits, "[", 2)
+unique(titles)
+
+# What's up with a title of 'the'?
+data.combined[which(titles == "the"),]
+
+# Re-map titles to be more exact
+titles[titles %in% c("Dona.", "the")] <- "Lady."
+titles[titles %in% c("Ms.", "Mlle.")] <- "Miss."
+titles[titles == "Mme."] <- "Mrs."
+titles[titles %in% c("Jonkheer.", "Don.")] <- "Sir."
+titles[titles %in% c("Col.", "Capt.", "Major.")] <- "Officer"
+table(titles)
+
+# Make title a factor
+data.combined$new.title <- as.factor(titles)
+
+# Visualize new version of title
+ggplot(data.combined[1:891,], aes(x = new.title, fill = Survived)) +
+  geom_bar() +
+  facet_wrap(~ Pclass) + 
+  ggtitle("Surival Rates for new.title by pclass")
+#
+
+# Collapse titles based on visual analysis
+indexes <- which(data.combined$new.title == "Lady.")
+data.combined$new.title[indexes] <- "Mrs."
+
+indexes <- which(data.combined$new.title == "Dr." | 
+                   data.combined$new.title == "Rev." |
+                   data.combined$new.title == "Sir." |
+                   data.combined$new.title == "Officer")
+data.combined$new.title[indexes] <- "Mr."
+#
+# Visualize 
+ggplot(data.combined[1:891,], aes(x = new.title, fill = Survived)) +
+  geom_bar() +
+  facet_wrap(~ Pclass) +
+  ggtitle("Surival Rates for Collapsed new.title by pclass")
+# Grab features
+features <- c("Pclass", "new.title", "family.size")
+rpart.train.2 <- data.combined[1:891, features]
+# Run CV and check out results
+rpart.2.cv.1 <- rpart.cv(94622, rpart.train.2, rf.label, ctrl.3)
+rpart.2.cv.1
+
+# Plot
+prp(rpart.2.cv.1$finalModel, type = 0, extra = 1, under = TRUE)
+#
+#
+#....
+#
+#
+# First, let's explore our collection of features using mutual information to
+# gain some additional insight. Our intuition is that the plot of our tree
+# should align well to the definition of mutual information.
+#install.packages("infotheo")
+library(infotheo)
+mutinformation(rf.label, data.combined$Pclass[1:891])
+mutinformation(rf.label, data.combined$Sex[1:891])
+mutinformation(rf.label, data.combined$SibSp[1:891])
+mutinformation(rf.label, data.combined$Parch[1:891])
+#mutinformation(rf.label, discretize(data.combined$Fare[1:891]))
+mutinformation(rf.label, data.combined$Embarked[1:891])
+mutinformation(rf.label, data.combined$title[1:891])
+mutinformation(rf.label, data.combined$family.size[1:891])
+mutinformation(rf.label, data.combined$ticket.first.char[1:891])
+#mutinformation(rf.label, data.combined$cabin.multiple[1:891])
+mutinformation(rf.label, data.combined$new.title[1:891])
+mutinformation(rf.label, data.combined$ticket.party.size[1:891])
+mutinformation(rf.label, discretize(data.combined$avg.fare[1:891]))
+# OK, now let's leverage the tsne algorithm to create a 2-D representation of our data 
+# suitable for visualization starting with folks our model gets right very often - folks
+# with titles other than 'Mr."
+#install.packages("Rtsne")
+library(Rtsne)
+most.correct <- data.combined[data.combined$new.title != "Mr.",]
+indexes <- which(most.correct$Survived != "None")
+# NOTE - Bug fix for original version. Rtsne needs a seed to ensure consistent
+# output between runs.
+set.seed(984357)
+tsne.1 <- Rtsne(most.correct[, features], check_duplicates = FALSE)
+ggplot(NULL, aes(x = tsne.1$Y[indexes, 1], y = tsne.1$Y[indexes, 2], 
+                 color = most.correct$survived[indexes])) +
+  geom_point() +
+  labs(color = "Survived") +
+  ggtitle("tsne 2D Visualization of Features for new.title Other than 'Mr.'")
+# To get a baseline, let's use conditional mutual information on the tsne X and
+# Y features for females and boys in 1st and 2nd class. The intuition here is that
+# the combination of these features should be higher than any individual feature
+# we looked at above.
+condinformation(most.correct$Survived[indexes], discretize(tsne.1$Y[indexes,]))
+
+
+# OK, now let's take a look at adult males since our model has the biggest 
+# potential upside for improving (i.e., the tree predicts incorrectly for 86
+# adult males). Let's visualize with tsne.
+misters <- data.combined[data.combined$new.title == "Mr.",]
+indexes <- which(misters$Survived != "None")
+
+tsne.2 <- Rtsne(misters[, features], check_duplicates = FALSE)
+ggplot(NULL, aes(x = tsne.2$Y[indexes, 1], y = tsne.2$Y[indexes, 2], 
+                 color = misters$Survived[indexes])) +
+  geom_point() +
+  labs(color = "Survived") +
+  ggtitle("tsne 2D Visualization of Features for new.title of 'Mr.'")
+
+
+# Now conditional mutual information for tsne features for adult males
+condinformation(misters$Survived[indexes], discretize(tsne.2$Y[indexes,]))
+
+
+#
+# Idea - How about creating tsne featues for all of the training data and
+# using them in our model?
+#
+tsne.3 <- Rtsne(data.combined[, features], check_duplicates = FALSE)
+ggplot(NULL, aes(x = tsne.3$Y[1:891, 1], y = tsne.3$Y[1:891, 2], 
+                 color = data.combined$Survived[1:891])) +
+  geom_point() +
+  labs(color = "Survived") +
+  ggtitle("tsne 2D Visualization of Features for all Training Data")
+
+# Now conditional mutual information for tsne features for all training
+condinformation(data.combined$Survived[1:891], discretize(tsne.3$Y[1:891,]))
+
