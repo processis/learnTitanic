f2c0f59ed1a253623ff2fcabc1c51093d8c63b16
Debian Live user <user@localhost.localdomain>
2025-01-22 07:35:37 +0000

    update

--- the big R/test2/server.R
+++ the big R/test2/server.R
@@ -1,23 +1,44 @@
-# 创建Shiny应用程序服务器
-server <- function(input, output) {
+server <- function(input, output, session) {
+  onSessionEnded(stopApp)
+  data <- reactive({
+    req(input$file1)
+    df <- read.csv(input$file1$datapath, header = input$header, sep = input$sep, quote = input$quote)
+    return(df)
+  })
   
-  data<- read.csv(inFile$datapath)
-  # 定义绘图函数
-  output$residualPlot <- renderPlot({
-    # 从用户输入中获取数据（假设数据在输入中名为'data'）
-    data <- input$data
-    
-    # 进行线性回归并提取残差
-    fit <- lm(y ~ x, data = data)
-    residuals <- resid(fit)
-    
-    # 创建散点图并添加回归线和残差点
-    plot <- ggplot(data, aes(x = x, y = y)) +
-      geom_point() +
-      geom_smooth(method = "lm", se = FALSE) +
-      geom_point(aes(y = residuals), color = "red")
-    
-    # 返回绘制好的图表
-    plot
+  output$contents <- renderTable({
+    if (input$disp == "head") {
+      return(head(data()))
+    }
+    else {
+      return(data())
+    }
+  })
+  output$summar <- renderPrint({
+    req(input$file1)
+    summary(data())
+  }
+  )
+  
+  output$variable_x <- renderUI({
+    selectInput("variableNames_x", label = "Variable_X", choices = names(data()))  
+  })
+  output$variable_y <- renderUI({
+    selectInput("variableNames_y", label = "Variable_Y", choices = names(data()) ) 
+  })
+  dat <- reactive({
+    test <- data.frame(data()[[input$variableNames_x]], data()[[input$variableNames_y]])
+    colnames(test) <- c("X", "Y")
+    return(test)
+  })
+  
+  output$plot <- renderPlot({
+    if (is.null(data)) { return(NULL)
+    } else {
+      ggplot(dat(),aes(x = X,y = Y)) + geom_point(colour = 'red') +
+        labs(y = input$variableNames_y,
+             x = input$variableNames_x,
+             title = "ggplot")
+    }
   })
 }
--- the big R/test2/ui.R
+++ the big R/test2/ui.R
@@ -1,25 +1,45 @@
-ibrary(shiny)
+library(shiny)
 library(ggplot2)
 
-# 创建Shiny应用程序界面
 ui <- fluidPage(
-  
-  sidebarLayout(
-    sidebarPanel(
-      # 在侧边栏中添加用户输入控件（如数据上传或选择）
-      # 这里省略了用户输入的部分
-      div(
-        fileInput("csvFile", "Choose defferential analysis result File",
-                  accept = c(
-                    "text/csv",
-                    "text/comma-separated-values,text/plain",
-                    ".csv")
-        )
-    ),
-    )
-    mainPanel(
-      # 在主面板中添加绘图输出
-      plotOutput("residualPlot")
-    )
-  )
-)
+  navbarPage("User Interface:",tabPanel("Upload",
+                                        titlePanel("Uploading Files"),
+                                        sidebarLayout(
+                                          sidebarPanel(
+                                            fileInput("file1", "Choose CSV File",
+                                                      multiple = TRUE,
+                                                      accept = c("text/csv",
+                                                                 "text/comma-separated-values,text/plain",
+                                                                 ".csv")),
+                                            tags$hr(),
+                                            checkboxInput("header", "Header", TRUE),
+                                            radioButtons("sep", "Separator",
+                                                         choices = c(Comma = ",",
+                                                                     Semicolon = ";",
+                                                                     Tab = "\t"),
+                                                         selected = ","),
+                                            tags$hr(),
+                                            radioButtons("disp", "Display",
+                                                         choices = c(Head = "head",
+                                                                     All = "all"),
+                                                         selected = "head"),
+                                            radioButtons("quote", "Quote",
+                                                         choices = c(None = "",
+                                                                     "Double Quote" = '"',
+                                                                     "Single Quote" = "'"),
+                                                         selected = '"')),
+                                          mainPanel(
+                                            verbatimTextOutput("summar"),
+                                            tableOutput("contents")
+                                          ))), 
+             tabPanel("Graphing",
+                      titlePanel("Plotting Graphs"),
+                      sidebarLayout(
+                        sidebarPanel( uiOutput("variable_x"),
+                                      uiOutput("variable_y")),
+                        mainPanel(
+                          h3(textOutput("caption")),
+                          plotOutput("plot")
+                        )
+                      ))
+  ))
