9f9e29f51478f69518f3a4a467901d7154d17a99
Debian Live user <user@localhost.localdomain>
2025-02-26 06:10:53 +0000

    !desharnaisLogEffort77kaggle3noMissingTrain.csv

---
+++ the big R/dashar/desha0226-!desharnaisLogEffort77kaggle3.R
@@ -0,0 +1,5 @@
+#!desharnaisLogEffort77kaggle3noMissingTrain.csv
+
+desharnais <- read.table("/media/user/娱乐/learnTitanic/the big R/dashar/!desharnaisLogEffort77kaggle3noMissingTrain.csv",
+                         sep = ",", header = TRUE)
+
