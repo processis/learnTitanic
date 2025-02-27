881da1a8efa6b45fc053bf6248fff414e8e0a268
Debian Live user <user@localhost.localdomain>
2025-02-27 05:22:44 +0000

    cor ma

--- the big R/dashar/TEST1.R
+++ the big R/dashar/TEST1.R
@@ -7,17 +7,15 @@ cor_matrix <- cor(data, method = "pearson", use = "complete.obs")
 # 打印相关性矩阵
 print(cor_matrix)
 
-# 可视化相关性矩阵
+# 选择特定变量
+selected_vars <- data[, c("LogLength", "LogEffort", "LogPtsAjust","TeamExp","ManagerExp")]
+
+# 计算相关性矩阵
+cor_matrix <- cor(selected_vars, method = "pearson", use = "complete.obs")
+
+# 打印相关性矩阵
+print(cor_matrix)
+
+# 可视化相关性矩阵（可选）
 library(corrplot)
 corrplot(cor_matrix, method = "circle")
-
-# 计算特定变量之间的相关性
-correlation <- cor(data$LogLength, data$LogEffort, method = "pearson")
-print(correlation)
-
-# 计算相关性的显著性
-cor_test_result <- cor.test(data$var1, data$var2, method = "pearson")
-print(cor_test_result)
-
-# 保存相关性矩阵
-write.csv(cor_matrix, file = "correlation_matrix.csv")
--- the big R/dashar/desha0227-!desharnaisLogEffort77kaggle3-test1-4loglength+L12.R
+++ the big R/dashar/desha0227-!desharnaisLogEffort77kaggle3-test1-4loglength+L12.R
@@ -6,6 +6,12 @@ desharnais <- read.table("/media/user/娱乐/learnTitanic/the big R/dashar/!desh
                          sep = ",", header = TRUE)
 
 
+
+
+
+
+
+
 # 加载ggplot2包
 library(ggplot2)
 
@@ -35,6 +41,44 @@ ggplot(data, aes(x = data$LogPtsAjust, y = data$LogEffort, color = data$Language
        y = "LogEffort",
        color = "Language") +
   theme_minimal()
+
+
+
+
+# 读取数据
+data <- read.csv("/media/user/娱乐/learnTitanic/the big R/dashar/!desharnais123fillMedLog71TrainSet-loglength.csv")
+
+# 计算Pearson相关性矩阵
+cor_matrix <- cor(data, method = "pearson", use = "complete.obs")
+
+# 打印相关性矩阵
+print(cor_matrix)
+
+# 选择特定变量
+selected_vars <- data[, c("LogLength", "LogEffort", "LogPtsAjust","TeamExp","ManagerExp")]
+
+# 计算相关性矩阵
+cor_matrix <- cor(selected_vars, method = "pearson", use = "complete.obs")
+
+# 打印相关性矩阵
+print(cor_matrix)
+
+# 可视化相关性矩阵（可选）
+library(corrplot)
+corrplot(cor_matrix, method = "circle")
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
